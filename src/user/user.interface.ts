96f383886b956838461da4ac7c259cedfdbe0c02
amaterasu-infg <foreverwakandaa@gmail.com>
2024-12-03 21:09:24 +0600

    refactor(user): enum

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,6 +1,8 @@
+import { UserRole } from './enum/role.enum';
+
 export interface IUser {
   username: string;
   email: string;
   password: string;
-  role: 'Admin' | 'Guest';
+  role: UserRole;
 }
