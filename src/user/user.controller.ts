e761003fd7c7deadd4854ce496c6096fccc6cc1f
amaterasu-infg <foreverwakandaa@gmail.com>
2024-12-03 21:06:28 +0600

    feat(user):  new endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,12 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { userModel } from './user.model';
 import { UserNotFoundException } from '../exceptions/userNotFound.exception';
+import { IController } from '../interfaces/controller.interface';
+import { IUser } from './user.interface';
+import { validationMiddleware } from '../middleware/validation.middleware';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
 
-export class UserController {
+export class UserController implements IController {
   public path = '/user';
   public router = Router();
   private user = userModel;
@@ -13,17 +17,75 @@ export class UserController {
 
   public initializeRoutes() {
     this.router.get(`${this.path}/:id`, this.getUserById);
+    this.router.get(this.path, this.getAllUsers);
+    this.router.post(this.path, validationMiddleware(CreateUserDto), this.createUser);
+    this.router.patch(`${this.path}/:id`, validationMiddleware(UpdateUserDto), this.updateUser);
+    this.router.delete(`${this.path}/:id`, this.deleteUser);
   }
 
   private getUserById = async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id;
-    const userQuery = this.user.findById(id);
+    try {
+      const { id } = req.params;
 
-    const user = await userQuery;
-    if (user) {
+      const user = await this.user.findById(id);
+
+      if (!user) {
+        return next(new UserNotFoundException(id));
+      }
+
       res.send(user);
-    } else {
-      next(new UserNotFoundException(id));
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  private getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.user.find();
+      res.send(users);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  private createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userData: IUser = req.body;
+      const newUser = new userModel(userData);
+      await newUser.save();
+
+      res.status(201).send(newUser);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  private updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const userData: IUser = req.body;
+
+      const updatedUser = await this.user.findByIdAndUpdate(id, userData, { new: true });
+
+      if (!updatedUser) {
+        return next(new UserNotFoundException(id));
+      }
+      res.send(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  private deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const deletedUser = await this.user.findByIdAndDelete(id);
+      if (!deletedUser) {
+        return next(new UserNotFoundException(id));
+      }
+      res.status(204).send();
+    } catch (err) {
+      next(err);
     }
   };
 }
