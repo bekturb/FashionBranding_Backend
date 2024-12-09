222097e7615df8200c9834ceab1c76c408abd747
bekturb <bekkgboy2@gmail.com>
2024-12-09 20:32:55 +0600

    refactor(authentication)/refactor code of auth

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,38 +1,21 @@
-import * as bcrypt from "bcrypt";
-import mongoose, { Error } from "mongoose";
 import { Request, Response, NextFunction, Router } from "express";
 import { IController } from "../interfaces/controller.interface";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateUserDto } from "../user/user.dto";
-import { userModel } from "../user/user.model";
-import AuthenticationService from "./authentication.service";
 import { LogInDto } from "./login.dto";
-import { NotFoundException } from "../exceptions/notfound.exception";
-import { otpCodeModel, verificationCodeModel } from "./verification.model";
-import { UserNotFoundException } from "../exceptions/userNotFound.exception";
-import VerificationsService from "./verifications.service";
-import EmailService from "./email.service";
-import WrongCredentialsException from "../exceptions/WrongCredentialsException";
-import TokenManager from "../utils/jwt";
-import CookiesManager from "../utils/cookies";
-import DataStoredInToken from "interfaces/dataStoredInToken";
 import { ResetPasswordDto } from "./resetPassword.dto";
 import { ForgetPasswordDto } from "./forgetPassword.dto";
-import { fiveMinutesAgo, oneHourFromNow, oneYearFromNow } from "../utils/date";
-import VerificationCode from "./enum/verificationCode.enum";
 import { ResetForgotenPasswordDto } from "./resetForgotenPassword.dto";
+import AuthenticationService from "./authentication.service";
+import VerificationsService from "./verifications.service";
+import CookiesManager from "../utils/cookies";
 
 export class AuthenticationController implements IController {
   public path = "/auth";
   public router = Router();
   public authenticationService = new AuthenticationService();
   public verificationService = new VerificationsService();
-  private emailService = new EmailService();
-  private tokenManager = new TokenManager();
   private cookiesManager = new CookiesManager();
-  private user = userModel;
-  private verificationCode = verificationCodeModel;
-  private otp = otpCodeModel;
 
   constructor() {
     this.initializeRoutes();
@@ -64,7 +47,7 @@ export class AuthenticationController implements IController {
     this.router.post(
       `${this.path}/forget/password`,
       validationMiddleware(ForgetPasswordDto),
-      this.sendPasswordResetEmail
+      this.sendPasswordReset
     );
     this.router.post(
       `${this.path}/reset/forgoten/password`,
@@ -92,34 +75,12 @@ export class AuthenticationController implements IController {
     response: Response,
     next: NextFunction
   ) => {
+    const { verificationId } = request.params;
     try {
-      const { verificationId } = request.params;
-
-      if (!mongoose.Types.ObjectId.isValid(verificationId)) {
-        return next(new NotFoundException("Invalid Id"));
-      }
-
-      const validCode = await this.verificationCode.findOne({
-        _id: verificationId,
-        type: VerificationCode.EmailVerification,
-        expiresAt: { $gt: new Date() },
-      });
-
-      if (!validCode) {
-        return next(
-          new NotFoundException("Invalid or expired verification code")
-        );
-      }
-
-      const user = await this.user.findById(validCode.userId);
-      if (!user) {
-        return next(new UserNotFoundException(String(validCode.userId)));
-      }
-
-      const otp = await this.verificationService.generateOtpCode(user);
-      await this.emailService.sendVerificationOtp(user.email, otp);
-
-      await validCode.deleteOne();
+      const { user } = await this.authenticationService.firstStepVerification(
+        verificationId,
+        next
+      );
 
       response.status(200).send({
         message: `Verification OTP code sent to ${user.email}`,
@@ -134,41 +95,15 @@ export class AuthenticationController implements IController {
     response: Response,
     next: NextFunction
   ) => {
+    const { email, otpCode }: { email: string; otpCode: string } = request.body;
     try {
-      const { email, otpCode }: { email: string; otpCode: string } =
-        request.body;
 
-      const storedOtp = await this.otp.findOne({ email });
-      if (!storedOtp || new Date() > storedOtp.expiresAt) {
-        return next(new NotFoundException("Invalid or expired OTP code"));
-      }
-
-      const isValidOtp = bcrypt.compareSync(otpCode, storedOtp.otp);
-      if (!isValidOtp) {
-        return next(new NotFoundException("Invalid OTP"));
-      }
-
-      const user = await this.user.findOneAndUpdate(
-        { email },
-        { isEmailConfirmed: true },
-        { new: true }
-      );
-
-      if (!user) {
-        return next(new NotFoundException("User not found"));
-      }
-      const refreshToken = this.tokenManager.signToken(
-        {
-          userId: user._id,
-        },
-        this.tokenManager.refreshTokenSignOptions
-      );
-
-      const accessToken = this.tokenManager.signToken({
-        userId: user._id,
-      });
-
-      await this.otp.deleteOne({ email });
+      const { user, accessToken, refreshToken } =
+        await this.authenticationService.secondStepVerification(
+          email,
+          otpCode,
+          next
+        );
 
       this.cookiesManager.setAuthCookies({
         response,
@@ -187,51 +122,11 @@ export class AuthenticationController implements IController {
     response: Response,
     next: NextFunction
   ) => {
-    try {
-      const logInData: LogInDto = request.body;
-      const user = await this.user.findOne({ email: logInData.email });
+    const logInData: LogInDto = request.body;
 
-      if (!user) {
-        next(new WrongCredentialsException());
-        return;
-      }
+    try {      
 
-      const isPasswordMatching = await bcrypt.compare(
-        logInData.password,
-        user.get("password", null, { getters: false })
-      );
-
-      if (!isPasswordMatching) {
-        next(new NotFoundException("Incorrect password. Please try again."));
-        return;
-      }
-
-      if (!user.isEmailConfirmed) {
-        const verificationCode = await this.verificationCode.create({
-          userId: user._id,
-          type: VerificationCode.EmailVerification,
-          expiresAt: oneYearFromNow(),
-        });
-        const url = `${process.env.APP_URL}/auth/email/verify/${verificationCode._id}`;
-
-        await this.emailService.sendVerificationEmail(user.email, url);
-
-        response.status(403).send({
-          message: `User is not verified. A new verification email has been sent to ${user.email}.`,
-        });
-        return;
-      }
-
-      const refreshToken = this.tokenManager.signToken(
-        {
-          userId: user._id,
-        },
-        this.tokenManager.refreshTokenSignOptions
-      );
-
-      const accessToken = this.tokenManager.signToken({
-        userId: user._id,
-      });
+      const { accessToken, refreshToken, user } = await this.authenticationService.login(logInData, response, next)
 
       this.cookiesManager.setAuthCookies({
         response,
@@ -266,38 +161,7 @@ export class AuthenticationController implements IController {
     try {
       const refreshToken = request.cookies.refreshToken as string | undefined;
 
-      if (!refreshToken) {
-        return next(new NotFoundException("Refresh token is missing."));
-      }
-
-      const { payload, error } =
-        this.tokenManager.verifyToken<DataStoredInToken>(refreshToken, {
-          secret: this.tokenManager.refreshTokenSignOptions.secret,
-        });
-
-      if (error) {
-        return next(new NotFoundException("Invalid or expired refresh token."));
-      }
-
-      if (!payload) {
-        return next(new NotFoundException("Invalid token payload."));
-      }
-
-      const userId = payload.userId;
-      const user = await this.user.findById(userId);
-      if (!user) {
-        return next(new UserNotFoundException(userId));
-      }
-
-      const newAccessToken = this.tokenManager.signToken(
-        { userId: user._id },
-        this.tokenManager.accessTokenSignOptions
-      );
-
-      const newRefreshToken = this.tokenManager.signToken(
-        { userId: user._id },
-        this.tokenManager.refreshTokenSignOptions
-      );
+      const { newAccessToken, newRefreshToken } = await this.authenticationService.refreshUserAccesToken(refreshToken, next)
 
       this.cookiesManager.setAuthCookies({
         response,
@@ -321,33 +185,9 @@ export class AuthenticationController implements IController {
     next: NextFunction
   ) => {
     try {
-      const {
-        email,
-        oldPassword,
-        password,
-        confirmPassword,
-      }: ResetPasswordDto = request.body;
+      const resetPasswordData: ResetPasswordDto = request.body;
 
-      const user = await this.user.findOne({ email });
-      if (!user) {
-        return next(new NotFoundException("User not found with this email"));
-      }
-
-      const isPasswordMatching = await bcrypt.compare(
-        oldPassword,
-        user.get("password", null, { getters: false })
-      );
-
-      if (!isPasswordMatching) {
-        return next(new NotFoundException("Incorrect old password"));
-      }
-
-      if (password !== confirmPassword) {
-        return next(new NotFoundException("Passwords do not match"));
-      }
-
-      user.password = await bcrypt.hash(password, 10);
-      await user.save();
+      await this.authenticationService.resetUserPassword(resetPasswordData, next)
 
       response.status(200).send({ message: "Password reset successfully" });
     } catch (error) {
@@ -355,41 +195,15 @@ export class AuthenticationController implements IController {
     }
   };
 
-  private sendPasswordResetEmail = async (
+  private sendPasswordReset = async (
     request: Request,
     response: Response,
     next: NextFunction
   ) => {
     try {
-      const { email }: ForgetPasswordDto = request.body;
+      const forgetPasswordData: ForgetPasswordDto = request.body;
 
-      const user = await this.user.findOne({ email });
-      if (!user)
-        return next(new NotFoundException("User not found with this email"));
-
-      const recentRequest = await this.verificationCode.countDocuments({
-        userId: user._id,
-        type: VerificationCode.PasswordReset,
-        createdAt: { $gt: fiveMinutesAgo() },
-      });
-
-      if (recentRequest >= 1) {
-        return next(
-          new NotFoundException("Too many requests, please try again later")
-        );
-      }
-
-      const expiresAt = oneHourFromNow();
-      const verificationCode = await this.verificationCode.create({
-        userId: user._id,
-        type: VerificationCode.PasswordReset,
-        expiresAt,
-      });
-
-      const url = `${process.env.APP_URL}/password/reset?code=${
-        verificationCode._id
-      }&exp=${expiresAt.getTime()}`;
-      await this.emailService.sendResetPassworUrl(user.email, url);
+      const { user } = await this.authenticationService.sendPasswordResetUrl(forgetPasswordData, next)
 
       response.status(200).send({
         message: `A password reset email has been sent to ${user.email}.`,
@@ -405,34 +219,9 @@ export class AuthenticationController implements IController {
     next: NextFunction
   ) => {
     try {
-      const { verificationCode, password }: ResetForgotenPasswordDto = request.body;
+      const resetForgotenPasswordData: ResetForgotenPasswordDto = request.body;
 
-      const validCode = await this.verificationCode.findOne({
-        _id: verificationCode,
-        type: VerificationCode.PasswordReset,
-        expiresAt: { $gt: new Date() },
-      });
-
-      if (!validCode) {
-        return next(
-          new NotFoundException("Invalid or expired verification code")
-        );
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const updatedUser = await this.user.findByIdAndUpdate(validCode.userId, {
-        password: hashedPassword,
-      });
-
-      if (!updatedUser) {
-        return next(
-          new NotFoundException("User not found or password reset failed")
-        );
-      }
-
-      await validCode.deleteOne();
-
+      await this.authenticationService.resetForgetPassword(resetForgotenPasswordData, next)
       this.cookiesManager.clearAuthCookies(response);
 
       response.status(200).send({ message: "Password reset successfully" });
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,23 +1,37 @@
-import UserWithThatEmailAlreadyExistsException from "../exceptions/UserWithThatEmailAlreadyExistsException";
-import * as bcrypt from "bcrypt";
+import { UserWithThatEmailAlreadyExistsException } from "../exceptions/userWithThatEmailAlreadyExists.exception";
+import { NotFoundException } from "../exceptions/notfound.exception";
+import { UserNotFoundException } from "../exceptions/userNotFound.exception";
+import { WrongCredentialsException } from "../exceptions/wrongCredentials.exception";
 import { CreateUserDto } from "../user/user.dto";
+import { LogInDto } from "./login.dto";
+import { ResetPasswordDto } from "./resetPassword.dto";
+import { ForgetPasswordDto } from "./forgetPassword.dto";
+import { ResetForgotenPasswordDto } from "./resetForgotenPassword.dto";
 import { userModel } from "../user/user.model";
-import EmailService from "./email.service";
-import { verificationCodeModel } from "./verification.model";
-import { oneYearFromNow } from "../utils/date";
+import { otpCodeModel, verificationCodeModel } from "./verification.model";
+import { compareValue, hashValue } from "../utils/bcrypt";
+import { fiveMinutesAgo, oneHourFromNow, oneYearFromNow } from "../utils/date";
+import TokenManager from "../utils/jwt";
 import VerificationCode from "./enum/verificationCode.enum";
+import DataStoredInToken from "../interfaces/dataStoredInToken";
+import VerificationsService from "./verifications.service";
+import EmailService from "./email.service";
+import { NextFunction, Response } from "express";
 
 class AuthenticationService {
   public user = userModel;
   public verificationCode = verificationCodeModel;
   private emailService = new EmailService();
-  public 
+  public verificationService = new VerificationsService();
+  private tokenManager = new TokenManager();
+  private otp = otpCodeModel;
+  public;
 
   public async register(userData: CreateUserDto) {
     if (await this.user.findOne({ email: userData.email })) {
       throw new UserWithThatEmailAlreadyExistsException(userData.email);
     }
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const hashedPassword = await hashValue(userData.password);
     const user = await this.user.create({
       ...userData,
       password: hashedPassword,
@@ -35,6 +49,255 @@ class AuthenticationService {
     return {
       user,
     };
+  }
+
+  public async firstStepVerification(
+    verificationId: string,
+    next: NextFunction
+  ) {
+    const validCode = await this.verificationCode.findOne({
+      _id: verificationId,
+      type: VerificationCode.EmailVerification,
+      expiresAt: { $gt: new Date() },
+    });
+
+    if (!validCode) {
+      next(new NotFoundException("Invalid or expired verification code"));
+    }
+
+    const user = await this.user.findById(validCode.userId);
+    if (!user) {
+      next(new UserNotFoundException(String(validCode.userId)));
+    }
+
+    const otp = await this.verificationService.generateOtpCode(user);
+    await this.emailService.sendVerificationOtp(user.email, otp);
+
+    await validCode.deleteOne();
+
+    return {
+      user,
+    };
+  }
+
+  public async secondStepVerification(
+    email: string,
+    otpCode: string,
+    next: NextFunction
+  ) {
+    const storedOtp = await this.otp.findOne({ email });
+    if (!storedOtp || new Date() > storedOtp.expiresAt) {
+      next(new NotFoundException("Invalid or expired OTP code"));
+    }
+
+    const isValidOtp = await compareValue(otpCode, storedOtp.otp);
+    if (!isValidOtp) {
+      next(new NotFoundException("Invalid OTP"));
+    }
+
+    const user = await this.user.findOneAndUpdate(
+      { email },
+      { isEmailConfirmed: true },
+      { new: true }
+    );
+
+    if (!user) {
+      next(new NotFoundException("User not found"));
+    }
+    const refreshToken = this.tokenManager.signToken(
+      {
+        userId: user._id,
+      },
+      this.tokenManager.refreshTokenSignOptions
+    );
+
+    const accessToken = this.tokenManager.signToken({
+      userId: user._id,
+    });
+
+    await this.otp.deleteOne({ email });
+
+    return {
+      user,
+      refreshToken,
+      accessToken,
+    };
+  }
+
+  public async login(
+    logInData: LogInDto,
+    response: Response,
+    next: NextFunction
+  ) {
+    const user = await this.user.findOne({ email: logInData.email });
+
+    if (!user) {
+      next(new WrongCredentialsException());
+    }
+
+    const isPasswordMatching = await compareValue(
+      logInData.password,
+      user.get("password", null, { getters: false })
+    );
+
+    if (!isPasswordMatching) {
+      next(new NotFoundException("Incorrect password. Please try again."));
+    }
+
+    if (!user.isEmailConfirmed) {
+      const verificationCode = await this.verificationCode.create({
+        userId: user._id,
+        type: VerificationCode.EmailVerification,
+        expiresAt: oneYearFromNow(),
+      });
+      const url = `${process.env.APP_URL}/auth/email/verify/${verificationCode._id}`;
+
+      await this.emailService.sendVerificationEmail(user.email, url);
+
+      response.status(403).send({
+        message: `User is not verified. A new verification email has been sent to ${user.email}.`,
+      });
+    }
+
+    const refreshToken = this.tokenManager.signToken(
+      {
+        userId: user._id,
+      },
+      this.tokenManager.refreshTokenSignOptions
+    );
+
+    const accessToken = this.tokenManager.signToken({
+      userId: user._id,
+    });
+
+    return { accessToken, refreshToken, user }
+  }
+
+  public async refreshUserAccesToken (refreshToken: string, next: NextFunction){
+    if (!refreshToken) {
+      next(new NotFoundException("Refresh token is missing."));
+    }
+
+    const { payload, error } =
+      this.tokenManager.verifyToken<DataStoredInToken>(refreshToken, {
+        secret: this.tokenManager.refreshTokenSignOptions.secret,
+      });
+
+    if (error) {
+      next(new NotFoundException("Invalid or expired refresh token."));
+    }
+
+    if (!payload) {
+      next(new NotFoundException("Invalid token payload."));
+    }
+
+    const userId = payload.userId;
+    const user = await this.user.findById(userId);
+    if (!user) {
+      next(new UserNotFoundException(userId));
+    }
+
+    const newAccessToken = this.tokenManager.signToken(
+      { userId: user._id },
+      this.tokenManager.accessTokenSignOptions
+    );
+
+    const newRefreshToken = this.tokenManager.signToken(
+      { userId: user._id },
+      this.tokenManager.refreshTokenSignOptions
+    );
+
+    return {
+      newAccessToken,
+      newRefreshToken
+    }
+  }
+
+  public async resetUserPassword (resetPasswordData: ResetPasswordDto, next: NextFunction, ){
+
+    const {email, oldPassword, password, confirmPassword} = resetPasswordData;
+
+    const user = await this.user.findOne({ email });
+      if (!user) {
+        next(new NotFoundException("User not found with this email"));
+      }
+
+      const isPasswordMatching = await compareValue(
+        oldPassword,
+        user.get("password", null, { getters: false })
+      );
+
+      if (!isPasswordMatching) {
+        next(new NotFoundException("Incorrect old password"));
+      }
+
+      if (password !== confirmPassword) {
+        next(new NotFoundException("Passwords do not match"));
+      }
+
+      user.password = await hashValue(password);
+      await user.save();
+  }
+
+  public async sendPasswordResetUrl (forgetPasswordData: ForgetPasswordDto, next){
+    const { email } = forgetPasswordData;
+
+    const user = await this.user.findOne({ email });
+      if (!user) next(new NotFoundException("User not found with this email"));
+
+      const recentRequest = await this.verificationCode.countDocuments({
+        userId: user._id,
+        type: VerificationCode.PasswordReset,
+        createdAt: { $gt: fiveMinutesAgo() },
+      });
+
+      if (recentRequest >= 1) {
+        next(
+          new NotFoundException("Too many requests, please try again later")
+        );
+      }
+
+      const expiresAt = oneHourFromNow();
+      const verificationCode = await this.verificationCode.create({
+        userId: user._id,
+        type: VerificationCode.PasswordReset,
+        expiresAt,
+      });
+
+      const url = `${process.env.APP_URL}/password/reset?code=${
+        verificationCode._id
+      }&exp=${expiresAt.getTime()}`;
+      await this.emailService.sendResetPassworUrl(user.email, url);
+
+      return {
+        user
+      }
+  }
+
+  public async resetForgetPassword (resetForgotenPasswordData: ResetForgotenPasswordDto, next: NextFunction){
+          const { verificationCode, password } = resetForgotenPasswordData;
+
+    const validCode = await this.verificationCode.findOne({
+      _id: verificationCode,
+      type: VerificationCode.PasswordReset,
+      expiresAt: { $gt: new Date() },
+    });
+
+    if (!validCode) {
+      next(new NotFoundException("Invalid or expired verification code"));
+    }
+
+    const hashedPassword = await hashValue(password);
+
+    const updatedUser = await this.user.findByIdAndUpdate(validCode.userId, {
+      password: hashedPassword,
+    });
+
+    if (!updatedUser) {
+      next(new NotFoundException("User not found or password reset failed"));
+    }
+
+    await validCode.deleteOne();
   }
 }
 
