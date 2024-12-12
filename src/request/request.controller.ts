c970f1c796e1154bc741b38110d75937b71dfc2a
bekturb <bekkgboy2@gmail.com>
2024-12-12 16:47:44 +0600

    refactor(request)/improved request's code

--- src/request/request.controller.ts
+++ src/request/request.controller.ts
@@ -1,12 +1,11 @@
-import { FilterQuery } from "mongoose";
 import { NextFunction, Request, Response, Router } from "express";
 import { requestModel } from "./request.model";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateRequestDto } from "./request.dto";
 import { IController } from "../interfaces/controller.interface";
-import { IRequestsQuery } from "interfaces/requestsQuery.interface";
-import { IRequest } from "./request.interface";
+import { IRequestsQuery } from "../interfaces/requestsQuery.interface";
 import { ApplicationRequestNotFoundException } from "../exceptions/applicationRequestNotFound.exception";
+import { QueryBuilder } from "../utils/queryBuilder";
 
 export class RequestController implements IController {
   public path: string = "/request";
@@ -50,51 +49,15 @@ export class RequestController implements IController {
     next: NextFunction
   ) => {
     try {
-      const page = Math.max(parseInt(req.query.page || "1", 10), 1);
-      const limit = Math.max(parseInt(req.query.limit || "10", 10), 1);
-      const search = req.query.search?.trim() || "";
-      const startDate = req.query.startDate
-        ? new Date(req.query.startDate)
-        : undefined;
-      const endDate = req.query.endDate
-        ? new Date(req.query.endDate)
-        : undefined;
-      const type = req.query.type;
-      const skip = (page - 1) * limit;
 
-      const filterQuery: FilterQuery<IRequest> = {};
+      const queryBuilder = new QueryBuilder(req.query)
 
-      if (search) {
-        filterQuery.$or = [
-          { name: { $regex: search, $options: "i" } },
-          { phoneNumber: { $regex: search, $options: "i" } },
-        ];
-      }
-
-      if (
-        startDate &&
-        !isNaN(startDate.getTime()) &&
-        endDate &&
-        !isNaN(endDate.getTime())
-      ) {
-        filterQuery.createdAt = {
-          $gte: startDate,
-          $lte: new Date(endDate.setHours(23, 59, 59, 999)),
-        };
-      } else if (startDate && !isNaN(startDate.getTime())) {
-        filterQuery.createdAt = { $gte: startDate };
-      } else if (endDate && !isNaN(endDate.getTime())) {
-        filterQuery.createdAt = {
-          $lte: new Date(endDate.setHours(23, 59, 59, 999)),
-        };
-      }
-
-      if (type) {
-        filterQuery.type = type;
-      }
+      const skip = queryBuilder.getSkip();
+      const limit = queryBuilder.getLimit();
+      const filters = queryBuilder.getFilters();
 
       const requests = await this.request
-        .find(filterQuery)
+        .find(filters)
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit);
