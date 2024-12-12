54c81c8988e0e8ac05bb9663f8b28c269bd9e23a
bekturb <bekkgboy2@gmail.com>
2024-12-12 15:20:43 +0600

    refactor(server)/deleted NotificationController

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import 'dotenv/config';
 import { UserController } from './user/user.controller';
 import { App } from './app';
 import { RequestController } from './request/request.controller';
-import { NotificationController } from './notification/notification.controller';
 
 const app = new App([
     new UserController(),
     new RequestController(),
-    new NotificationController()
 ]);
 
 app.listen();
