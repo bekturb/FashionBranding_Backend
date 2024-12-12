7129bcc713e980cd2113b87f44142626f9e0f408
bekturb <bekkgboy2@gmail.com>
2024-12-12 14:02:13 +0600

    feat(server)/added NotificationController

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import 'dotenv/config';
 import { UserController } from './user/user.controller';
 import { App } from './app';
+import { RequestController } from './request/request.controller';
+import { NotificationController } from './notification/notification.controller';
 
-const app = new App([new UserController()]);
+const app = new App([
+    new UserController(),
+    new RequestController(),
+    new NotificationController()
+]);
 
 app.listen();
