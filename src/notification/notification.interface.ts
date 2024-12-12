d8a76bf3870ef09a4d4f6ad747f33e5c82862573
bekturb <bekkgboy2@gmail.com>
2024-12-12 14:58:54 +0600

    refactor(notification)/deleted notification

--- src/notification/notification.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-import Request from "../request/enum/request.enum";
-
-export interface INotification extends Document {
-  name: string;
-  type: Request;
-  seen: boolean;
-}
